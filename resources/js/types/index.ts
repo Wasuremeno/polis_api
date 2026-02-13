eed71febbba751cf1eb0520ce09c8feab066f141
Wasuremeno <wasuremono19@gmail.com>
2026-02-13 21:04:54 +0300

    feat: define TypeScript types for Article and Comment

--- resources/js/types/index.ts
+++ resources/js/types/index.ts
@@ -1,1 +1,21 @@
 export type * from './auth';
+
+
+export interface Article {
+    id: number;
+    title: string;
+    content: string;
+    created_at: string;
+    comments?: Comment[];
+}
+
+export interface Comment {
+    id: number;
+    author_name: string;
+    content: string;
+    created_at: string;
+}
+
+export interface PageProps {
+    [key: string]: unknown;
+}
