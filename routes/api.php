31ea12922c7eb371848cad927efd9fdec0a69d37
Wasuremeno <wasuremono19@gmail.com>
2026-02-13 20:57:45 +0300

    feat: define web and API routes

---
+++ routes/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ArticleController;
+use App\Http\Controllers\Api\CommentController;
+
+Route::prefix('v1')->group(function () {
+    Route::apiResource('articles', ArticleController::class)->only(['index', 'show', 'store']);
+    Route::post('/articles/{article}/comments', [CommentController::class, 'store']);
+});
--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,17 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('welcome');
-})->name('home');
+    return Inertia::render('Home');
+});
+
+Route::get('/articles', function () {
+    return Inertia::render('Articles/Index');
+})->name('articles.index');
+
+Route::get('/articles/{id}', function ($id) {
+    return Inertia::render('Articles/Show', ['id' => $id]);
+})->name('articles.show');
+
+Route::get('/articles/create', function () {
+    return Inertia::render('Articles/Create');
+})->name('articles.create');
