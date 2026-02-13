05b17cffb7410debf392b04f6cf864fe70994cc0
Wasuremeno <wasuremono19@gmail.com>
2026-02-13 20:59:14 +0300

    feat: define model relationships and fillable attributes

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Article extends Model
 {
-    //
+    protected $fillable = ['title', 'content'];
+
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
