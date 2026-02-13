66c2d0910c17206bb9854eeab61d7d3d52b60d6f
Wasuremeno <wasuremono19@gmail.com>
2026-02-13 20:56:24 +0300

    feat: setup Inertia React with SSR support

--- resources/js/ssr.tsx
+++ resources/js/ssr.tsx
@@ -1,16 +1,20 @@
+import React from 'react';
 import { createInertiaApp } from '@inertiajs/react';
-import createServer from '@inertiajs/react/server';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { createServer } from '@inertiajs/react/server';
 import { renderToString } from 'react-dom/server';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = 'Laravel';
 
 createServer((page) =>
     createInertiaApp({
         page,
         render: renderToString,
-        title: (title) => (title ? `${title} - ${appName}` : appName),
-        resolve: (name) => resolvePageComponent(`./pages/${name}.tsx`, import.meta.glob('./pages/**/*.tsx')),
+        resolve: (name) => resolvePageComponent(
+            `./Pages/${name}.tsx`,
+            import.meta.glob('./Pages/**/*.tsx')
+        ),
         setup: ({ App, props }) => <App {...props} />,
-    }),
+        title: (title) => title ? `${title} - ${appName}` : appName,
+    })
 );
